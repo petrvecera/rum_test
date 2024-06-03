048342b8b63fad9aabe424fe97e666922a96f92b
Petr Vecera <vecera.petr@gmail.com>
2024-06-03 19:42:01 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+import { Metrics } from "@edgio/rum";
+
+new Metrics({
+  token: "cdbeca7f-be87-45d8-a36a-0ba520f1ec5c",
+}).collect();
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
